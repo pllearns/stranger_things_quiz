35dedd2c3b090829057c0bdf11e2f2ab9cf20fff
Rachel Ralston <rachelralston@gmail.com>
2016-09-12 14:59:28 -0700

    added basic schema

---
+++ schema.sql
@@ -0,0 +1,46 @@
+-- CREATE TABLES
+
+--psql bookstore < schema.sql
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS quiz_sessions;
+DROP TABLE IF EXISTS quiz_session_questions;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS answers;
+
+CREATE TABLE users
+(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE quiz_sessions
+(
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  started_at current_timestamp
+);
+
+CREATE TABLE quiz_session_questions
+(
+  id SERIAL PRIMARY KEY,
+  quiz_session_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+  correct BOOLEAN
+);
+
+CREATE TABLE questions
+(
+  id SERIAL PRIMARY KEY,
+  question_text TEXT NOT NULL,
+  img_url VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE answers
+(
+  id SERIAL PRIMARY KEY,
+  question_id INTEGER NOT NULL,
+  answer_text INTEGER NOT NULL,
+  correct BOOLEAN
+);
