11a55071a849c691c4c8cfdd820d4fab7ba76c3c
Phillip Lorenzo <pllearns@gmail.com>
2016-09-13 08:55:04 -0700

    Index route added (#9)
    * Started routes
    
    * Removing dist from tracking

--- dist/public/javascripts/main.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var _lodash = require('lodash');
-
-var _lodash2 = _interopRequireDefault(_lodash);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
