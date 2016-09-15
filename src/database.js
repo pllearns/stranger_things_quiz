dde4ccb6846ec696cff00606946879315990fe1a
Phillip Lorenzo <pllearns@gmail.com>
2016-09-15 15:10:54 -0700

    WIP: Still working on the database logic

--- src/database.js
+++ src/database.js
@@ -23,7 +23,7 @@ const getQuizSession = 'SELECT * FROM quiz_sessions WHERE id=$1'
 const getQuestion = 'SELECT * FROM questions WHERE id IN ($1:csv)'
 const getAllQuestions = 'SELECT * FROM questions'
 const getAllAnswers = 'SELECT * FROM answers WHERE question_id IN ($1:csv)'
-const getAnswersSingleQuestion = 'SELECT * FROM answers WHERE question_id=$1'
+const getOneQuestAnswer = 'SELECT * FROM answers WHERE question_id=$1'
 
 const Quiz = {
   createSession: (user_id) => db.one( createSession, [ user_id ]),
@@ -31,7 +31,7 @@ const Quiz = {
   getAllQuestions: question_id => db.any( getAllQuestions, [ question_id ]),
   getQuestion: quiz_session_id => db.any( getQuestion, [ quiz_session_id ]),
   getAllAnswers: question_id => db.any(getAllAnswers, [ question_id]),
-  getAnswersSingleQuestion: question_id => db.any(getAnswersSingleQuestion, [question_id],
+  getOneQuestAnswer: question_id => db.any(getOneQuestAnswer, [ question_id ])
 }
 
 // refactor so that one question and answer set is retrieved at a time
@@ -43,42 +43,40 @@ const getAllQuestionsByQuizSession = () => {
 
     return Promise.all([
       Quiz.getQuestion(questionIds),
-      Quiz.getAnswersSingleQuestion(questionIds)
+      Quiz.getAllAnswers(questionIds)
     ]).then(data => {
       const questions = data[0]
       const answers = data[1]
-      console.log(data)
+      // console.log(data)
 
       let questionAnswers = questions.map( question => {
-          answers.map( answer => {
-
-              let questionAnswer = answer.id
-              console.log(questionAnswer);
-
-              if( answer.question_id === question.id ) {
-                question.answers = [...questionAnswer]
-              }
-          })
+        let questionId = question.id
+        let allAnswers = Quiz.getOneQuestAnswer(questionId)
+        console.log(allAnswers)
+        answers.map( answer => {
+          let questionAnswer = answer.id
+            if( answer.question_id === question.id ) {
+              question.answers = [...questionAnswer]
+            }
+        })
       })
-
-      // const individualQuizQuestAndAns = []
-      // const questionAnswers = questions.map(question => {
-      //   const q_id = question.id
-      //   individualQuizQuestAndAns.push( question )
-      //   for each (q_id){
-      //     answers.map(answer => {
-      //       if (answer.question_id === q_id) {
-      //       individualQuizQuestAndAns.push(answer.question_id)
-      //       }
-      //     })
-      //   }
-      // })
-      console.log( 'questionAnswers', questionAnswers )
-
-      return data
-      // return db.One(question, answer)
+      return questionAnswers
     })
   })
 }
 
 export { User, Quiz , getAllQuestionsByQuizSession }
+
+
+// const individualQuizQuestAndAns = []
+// const questionAnswers = questions.map(question => {
+//   const q_id = question.id
+//   individualQuizQuestAndAns.push( question )
+//   for each (q_id){
+//     answers.map(answer => {
+//       if (answer.question_id === q_id) {
+//       individualQuizQuestAndAns.push(answer.question_id)
+//       }
+//     })
+//   }
+// })
