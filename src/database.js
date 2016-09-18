324caf7381e0af47a0ad7155ccbdcc96b3be64e4
Rachel <rachelralston@gmail.com>
2016-09-18 15:56:06 -0700

    Scoring (#20)
    * WIP: started putting together and rendering scoring logic
    
    * Results route finished, WIP: scoring logic
    
    * WIP: figureing out scoring
    
    * Got true and false to return :D
    
    * got score logic working, it's not rendering a page correctly yet
    
    * cleaning up files
    
    * working on styling

--- src/database.js
+++ src/database.js
@@ -23,6 +23,7 @@ const getQuizSession = 'SELECT * FROM quiz_sessions WHERE id=$1'
 const getQuestion = 'SELECT * FROM questions WHERE id IN ($1:csv)'
 const getAllQuestions = 'SELECT * FROM questions'
 const getAllAnswers = 'SELECT answers.*, question_id FROM answers WHERE question_id IN ($1:csv)'
+const getCorrectAnswer = 'SELECT answers.*, question_id FROM answers WHERE question_id IN (1, 2, 3) AND correct=true'
 // const getOneQuestAnswer = 'SELECT * FROM answers WHERE question_id=$1'
 
 
@@ -32,7 +33,8 @@ const Quiz = {
   getAllQuestions: question_id => db.any( getAllQuestions, [ question_id ]),
   getQuestion: quiz_session_id => db.any( getQuestion, [ quiz_session_id ]),
   getAllAnswers: question_id => db.any(getAllAnswers, [ question_id]),
-  getOneQuestAnswer: question_id => db.any(getOneQuestAnswer, [ question_id ])
+  getOneQuestAnswer: question_id => db.any(getOneQuestAnswer, [ question_id ]),
+  getCorrectAnswers: question_id => db.any( getCorrectAnswer, [ question_id ] )
 }
 
 // refactor so that one question and answer set is retrieved at a time
@@ -50,58 +52,83 @@ const getAllQuestionsByQuizSession = () => {
       questions.forEach( question => {
         question.answers = answers.filter(answer => answer.question_id === question.id)
       })
-      console.log(questions[1].answers[0].answer_text);
       return questions
     })
   })
 }
 
-
-
-
-
-export { User, Quiz , getAllQuestionsByQuizSession }
-
-// const getAllQuestionsByQuizSession = () => {
-//   return Quiz.getAllQuestions().then( questions => {
-//     const questionIds = questions.map(question => question.id)
-//
-//     return Promise.all([
-//       Quiz.getQuestion(questionIds),
-//       Quiz.getAllAnswers(questionIds)
-//     ]).then(data => {
-//       const questions = data[0]
-//       const answers = data[1]
-//       let questionAnswers = questions.map( question => {
-//         let questionId = question.id
-//         let allAnswers = Quiz.getOneQuestAnswer(questionId)
-//         .then(getAnswerSet => {
-//           answers.map( answer => {
-//             let questionAnswer = answer.id
-//             if( answer.question_id === question.id ) {
-//               question.answers = [...questionAnswer]
-//             }
-//           })
-//         })
-//       })
-//       console.log(data);
-//       return data
-//     })
-//   })
+// const getScore = () => {
+//   console.log('this is the score?')
+//   let score = 0
+//   return score
 // }
 
+let sessionAnswers = new Array()
+// make this auto? add it in a function with looping and getting data from the database
+sessionAnswers["Eggos"] = 1
+sessionAnswers["Pancakes"] = 0
+sessionAnswers["The Upside Down"] = 1
+sessionAnswers["The Entryway"] = 0
+sessionAnswers["Hawkins, IN"] = 1
+sessionAnswers["Oakland, CA"] = 0
 
-
-
-// const individualQuizQuestAndAns = []
-// const questionAnswers = questions.map(question => {
-//   const q_id = question.id
-//   individualQuizQuestAndAns.push( question )
-//   for each (q_id){
-//     answers.map(answer => {
-//       if (answer.question_id === q_id) {
-//       individualQuizQuestAndAns.push(answer.question_id)
-//       }
-//     })
+// const getRightAnswer = () => {
+//   console.log('===================wat')
+//   let isItRight = 0
+//   let theForm = document.forms["quiz-form"]
+//   let selectedAnswer = theForm.elements["eggo1"] // does this need to change? do we need to do more to call this here???
+//   for(var i = 0; i < selectedAnswer.length; i++){
+//     if(selectedAnswer[i].checked){
+//       isItRight = sessionAnswers[selectedAnswer[i].value]
+//       break
+//     }
 //   }
-// })
+//   return isItRight
+// }
+
+// const getTotal = () => {
+//   let totalScore += getRightAnswer()
+//   console.log(totalScore)
+// }
+
+//looking to pull which thing was clicked from form
+//get data for what I"m looking for
+
+const correctCount = answers => {
+  let correctAnswers = 0
+  let totalAnswers = Object.keys(answers).length
+  for(let i = 0; i <= totalAnswers; i++){
+    if ( answers[i] === 'true'){
+      correctAnswers += 1
+    }
+  }
+  let percentCorrect = Math.floor((correctAnswers / totalAnswers) * 100)
+  return percentCorrect
+} 
+
+
+export { User, Quiz , getAllQuestionsByQuizSession, correctCount}
+
+
+//some ideas for counting score
+    // for (let i = 0; i < answers.length; i++) {
+    //     let currentQuestion = ;
+    //     let questionAnswers = document.getElementsByName(currentQuestion);
+    //     for (let j = 0; j < questionResponses.length; j++) {
+    //         if (questionResponses[i].checked) {
+    //             userInput[i] = question0s[i].value;
+    //         }
+    //     }
+    // }
+    // let score = 0
+    // // after this completes, you'll have the user input values
+    // // the rest of your code should now work
+    // for (let i=0;i<numQuestions;i++){
+    //     if (userInput[i]==answers[i]){
+    //         score += 1;
+    //     } else {
+    //         score += 0;
+    //     }
+    // }
+    //
+    // return score;
